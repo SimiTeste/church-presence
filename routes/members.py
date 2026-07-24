15e20fb97d449d4c6c7dc3800294d86c93c686c7
SimiProjetos <202503795282@alunos.estacio.br>
2026-07-23 23:37:50 -0300

    Atualiza rotas de membros para salvar e filtrar por departamento

--- routes/members.py
+++ routes/members.py
@@ -8,11 +8,18 @@ members_bp = Blueprint('members', __name__)
 @members_bp.route('/members')
 @login_required
 def index():
+    departamento_selecionado = request.args.get('departamento')
     page = request.args.get('page', 1, type=int)
     per_page = 10
-    pagination = Member.query.paginate(page=page, per_page=per_page, error_out=False)
+
+    query = Member.query
+    if departamento_selecionado:
+        query = query.filter_by(departamento=departamento_selecionado)
+        
+    pagination = query.paginate(page=page, per_page=per_page, error_out=False)
     members = pagination.items
-    return render_template('members.html', members=members, pagination=pagination)
+    
+    return render_template('members.html', members=members, pagination=pagination, departamento_selecionado=departamento_selecionado)
 
 @members_bp.route('/members/new', methods=['GET', 'POST'])
 @login_required
@@ -21,6 +28,7 @@ def new():
         nome = request.form.get('nome')
         cpf = request.form.get('cpf')
         telefone = request.form.get('telefone')
+        departamento = request.form.get('departamento')
         
         if not nome or not cpf:
             flash('Nome e CPF são obrigatórios.', 'danger')
@@ -31,7 +39,7 @@ def new():
             flash('Já existe um membro cadastrado com este CPF.', 'danger')
             return redirect(url_for('members.new'))
             
-        new_member = Member(nome=nome, cpf=cpf, telefone=telefone)
+        new_member = Member(nome=nome, cpf=cpf, telefone=telefone, departamento=departamento)
         db.session.add(new_member)
         db.session.commit()
         
@@ -49,6 +57,7 @@ def edit(id):
         member.nome = request.form.get('nome')
         member.cpf = request.form.get('cpf')
         member.telefone = request.form.get('telefone')
+        member.departamento = request.form.get('departamento')
         
         db.session.commit()
         flash('Membro atualizado com sucesso!', 'success')
